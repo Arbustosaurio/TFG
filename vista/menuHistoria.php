f04faa2248d1497e0769c5232ba7c653cbbc4286
Daniel <danielgasparcandela@gmail.com>
2025-04-30 12:05:22 +0200

    cambiado el sistema de ver la pagina, en vez de cookies con la BBDD y la tabla progreso

--- vista/menuHistoria.php
+++ vista/menuHistoria.php
@@ -33,34 +33,36 @@
 
         <form action="verHistoria.php" method="post">                
             <input type="hidden" name="historiaId" value= <?php echo $historiaId ?> >
+            <!-- Se le pasa una pagina porque la pagina verHistoria va a recibir la pagina de si misma al ir avanzando, y asi siempre recive un dato y evito problemas-->
+            <input type="hidden" name="paginaId" value= 0 >
 
-        <?php
-        // Comprobamos si se ha iniciado sesion, ya que si no, no aparece la opcion de usar personaje
-        if (session_id() === '') {
-            echo "<input type=\"hidden\" name=\"personajeId\" value=0 >";
-            echo "<label>Si desea usar personajes, inicie sesion</label>";
-        }
-        else{
-            // Si el usuario esta iniciado mostramos un desplegable con sus personajes a elegir
-            $sql = "SELECT id, nombre FROM personajes WHERE idUsuario='$_SESSION['id']'";
-            $personajes = mysqli_query($conexion, $sql);
-            ?>
-
-            <label for="personajeId">Selecciona un personaje:</label>
-            <select name="personajeId" id="personajeId" required>
-            <option value="0">-- Ninguno --</option>
-        <?php
-        // Genera las opciones
-        if ($personajes->num_rows > 0) {
-            while($fila = $personajes->fetch_assoc()) {
-                echo '<option value="'.$fila['id'].'">'.$fila['nombre'].'</option>';
-            }
-        }
-        ?>
-    </select>
             <?php
-        }
-        ?>
+            // Comprobamos si se ha iniciado sesion, ya que si no, no aparece la opcion de usar personaje
+            if (session_id() === '') {
+                echo "<input type=\"hidden\" name=\"personajeId\" value=0 >";
+                echo "<label>Si desea usar personajes, inicie sesion</label>";
+            }
+            else{
+                // Si el usuario esta iniciado mostramos un desplegable con sus personajes a elegir
+                $sql = "SELECT id, nombre FROM personajes WHERE idUsuario='$_SESSION['id']'";
+                $personajes = mysqli_query($conexion, $sql);
+                ?>
+
+                <label for="personajeId">Selecciona un personaje:</label>
+                <select name="personajeId" id="personajeId" required>
+                <option value="0">-- Ninguno --</option>
+            <?php
+            // Genera las opciones
+            if ($personajes->num_rows > 0) {
+                while($fila = $personajes->fetch_assoc()) {
+                    echo '<option value="'.$fila['id'].'">'.$fila['nombre'].'</option>';
+                }
+            }
+            ?>
+            </select>
+                <?php
+            }
+            ?>
             <button type="submit">Comenzar lectura</button>
         </form>
 
--- vista/verHistoria.php
+++ vista/verHistoria.php
@@ -6,8 +6,10 @@
     if(isset($_POST['historiaId'])){
         $historiaId = $_POST['historiaId'];
         $personajeId = $_POST['personajeId'];
+        $paginaId = $_POST['paginaId'];
     }
 
+    /*
     $nombreCookie = 'hist_' . $historiaId . '_pers_' . $personajeId;
 
     // Para saber la pagina en la que se encuentra creamos una cookie que guarda la ip de esta, si no esta ya creada
@@ -17,7 +19,22 @@
 
       } else {
         $paginaId = $_COOKIE[$nombreCookie];
-      }
+      }*/
 
-      
+      // Al iniciar la lectura, creamos la entrada en la BBDD del progreso si no esta ya creada
+      $sql = "SELECT id_pag_actual FROM progreso WHERE id_usuario='$_SESSION['id']' AND id_historia='$historiaId'";
+      $paginaIdBBDD = mysqli_query($conexion, $sql);
+
+      // Si no se encuentra una pagina con este progreso, se crea uno nuevo
+      if(mysql_num_rows($paginaId) == 0){
+        $sql = "INSERT INTO progreso (id_usuario, id_historia, id_personaje, id_pag_actual)
+            VALUES ('$$_SESSION['id']', '$historiaId', '$personajeId', 1)";
+
+        mysqli_query($conexion, $sql);
+      } 
+      // y si se encuentra el progreso, se saca la pagina para poder mostrarla
+      else{
+        $idBBDD = $paginaIdBBDD->fetch_assoc();
+        $paginaId = $idBBDD['id_pag_actual'];
+      }
 ?>
